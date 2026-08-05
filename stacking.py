1c6fa029accb96113a1a2a08d81f7e44f9d810d8
Legolas4224 <thomas.s.kirkpatrick@hotmail.com>
2026-08-05 23:04:05 +1000

    Adds script to calculate zenith coordinates

--- stacking.py
+++ stacking.py
@@ -9,6 +9,21 @@ import os
 from dng_to_fits import  convert_dng_to_fits, tiff_to_fits
 from helpers import calc_image_scale
 import subprocess
+import zenith_coords
+
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, AltAz, EarthLocation
+from datetime import datetime
+
+
+# ========== GLOBAL PARAMS ==================
+focal_length = 4.5 #mm
+pixel_size = 1.55 #microns
+
+raw_image_path = "/home/thomas/Documents/Code/QuadStar/platesolving/test_images/SkyTest3/0.5s_5/"
+raw_image_type = "tiff"
+
+# ===========================================
 
 def add_RADEC_to_fits(file, coordinates_dict) :
     obstime = datetime.now()
@@ -16,11 +31,11 @@ def add_RADEC_to_fits(file, coordinates_dict) :
     RA = coordinates_dict["RA"]
     DEC = coordinates_dict["DEC"]
 
-    image_scale = calc_image_scale(1.55, 4.5)
+    image_scale = calc_image_scale(pixel_size, focal_length)
 
     # 2. Add or update the RA and DEC keywords
     # Standard FITS format expects decimal degrees
-    header["RA"] = RA     # Center RA in degrees 17
+    header["RA"] = RA     # Center RA in DEGREES 17
     header["DEC"] = DEC     # Center Dec in degrees -42
 
     header["SECPIX"] = (image_scale, "image scale in arcsec/pixel")
@@ -52,15 +67,15 @@ def sigma_clipped_stack(frames, sigma=2.5):
 # ==========================================================================
 
 def main(raw_image_path, filetype) :
-    
+
+    # ======== Load Images ==========
     files = sorted(glob.glob(f"{raw_image_path}*.{filetype}"))
     print(f"Found {len(files)} frames")
 
     if not files:
         raise RuntimeError("No image files found.")
 
-    #tiff_to_fits()
-
+    # ======== Convert Raw images to FITS format =========
     now = datetime.now()
     fits_dir = (f"./{now}-fits/")
     os.mkdir(fits_dir)
@@ -77,13 +92,12 @@ def main(raw_image_path, filetype) :
             converted = tiff_to_fits(file, f"{fits_dir}/image{i}.fits")
             converted_files.append(converted)
 
+    # ========= Align Images =========
     reference = fits.getdata(converted_files[0]).astype(np.float32)
     frames = [reference] # np.array([fits.getdata(f).astype(np.float32) for f in files])
-
     print(f"Registering Frames...")
     i = 1
     for f in converted_files[1:]:
-        
         source = fits.getdata(f).astype(np.float32)
         # Returns the aligned image and the transformation used
         aligned, footprint = aa.register(source, reference)
@@ -91,8 +105,11 @@ def main(raw_image_path, filetype) :
         frames.append(aligned)
         i += 1
 
+    # ========= Integrate Aligned Images =========
     frames = np.stack(frames)
     result = sigma_clipped_stack(frames, sigma=2.5)
+
+    # ========= Save integrated image to folder =========
     try :
         os.mkdir("./stacked/")
     except :
@@ -100,14 +117,16 @@ def main(raw_image_path, filetype) :
     output_path = f'./stacked/{datetime.now()}.fits'
     fits.writeto(output_path, result, overwrite=True)
     print(f"Integration Complete: Image saved as {output_path}")
+
     # ========= Add coords, time, image scale to stacked file ==========
+    coordinates = zenith_coords.main()
     coordinates_dict = {
-        "RA" : 255.0,
-        "DEC" : -42.0
+        "RA" : coordinates.ra,       #255.0, # THIS NEEDS TO BE IN DEGREES, NOT HOURS
+        "DEC" : coordinates.dec        #-42.0
     }
     add_RADEC_to_fits(output_path, coordinates_dict)
 
-    # Run ASTAP
+    # ========= Run ASTAP =========
     ASTAP_PROG_NAME: str = "astap" #_cli"
     try:
         result = subprocess.run([ASTAP_PROG_NAME, "-f", output_path, "-log", "-d /home/thomas/Documents/Code/QuadStar/platesolving/ASTAP_DB" ])
@@ -117,36 +136,7 @@ def main(raw_image_path, filetype) :
     except subprocess.TimeoutExpired:
         print("Timeout")
 
-    #ra_str, dec_str = get_ra_dec(file_name + ".log")
-#
-    #try:
-    #    if ra_str is None or dec_str is None:
-    #        print("No plate solve found.")
-    #    else:
-    #        ra_hrs = ra_to_degrees(ra_str) / 15.0
-    #        dec_degs = dec_to_degrees(dec_str)
-    #        print(f"RA : {ra_to_degrees(ra_str)}")
-    #        print(f"DEC: {dec_to_degrees(dec_str)}")
-#
-    #        vec = position_of_radec(ra_hrs, dec_degs, t=t)
-    #        print(f"skyfield Vec: {vec.distance()}")
-#
-    #        ecef = vec.frame_xyz(itrs).km
-    #        ecef_norm = ecef / np.linalg.norm(ecef)
-    #        print(f"ecef: {ecef}")
-#
-    #        # NOTE: Gravity is ~ 9.69m/s^2 at 40km, calculate this further
-#
-    #        gx = 0.95
-    #        gy = gz = 0.22079
-    #        grav = np.array([gx, gy, gz])
-    #        grav_norm = grav / np.linalg.norm(grav)
-#
-    #except Exception as e:
-    #    print(f"Failed to convert ra or dec: {e}")
 
 if __name__ == "__main__" :
-    
-
-    main("/home/thomas/Documents/Code/QuadStar/platesolving/test_images/SkyTest3/0.5s_5/", "tiff")
+    main(raw_image_path, raw_image_type)
     
---
+++ zenith_coords.py
@@ -0,0 +1,32 @@
+
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, AltAz, EarthLocation
+from datetime import datetime
+
+
+def make_EarthLocation(coords_dict) :
+    lat = coords_dict["latitude"]
+    lon = coords_dict["longitude"]
+    location = EarthLocation.from_geodetic(lon, lat, height=300)
+    #print(location)
+    return location
+
+def estimate_zenith_coords(time, location) :
+    altaz_frame = AltAz(obstime=time, location=location)
+    zenith = SkyCoord(0.0,90.0, frame=altaz_frame, unit="deg")
+    zenith_equatorial = zenith.transform_to("icrs")
+    print(f"Zenith Coordinates for location {location} at time {time}:")
+    print(f"RA: {zenith_equatorial.ra}")
+    print(f"DEC: {zenith_equatorial.dec}")
+    return zenith_equatorial
+
+def main() :
+    
+    coord_dict = {
+        "latitude" : -27.8305,
+        "longitude" : 142.6080
+    } 
+    time = Time.now()
+    location = make_EarthLocation(coord_dict)
+    return estimate_zenith_coords(time, location)
+    
